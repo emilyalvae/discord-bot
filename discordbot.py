dd3ff7deef8020298db3be886bcd349ee8a9abb6
emilyalvae <emilisa23@gmail.com>
2023-07-10 00:18:47 -0500

    token priv

--- discordbot.py
+++ discordbot.py
@@ -2,19 +2,18 @@
 from discord_easy_commands import EasyBot
 import pyjokes
 import discord
-
-
-
-token =  "MTEyNjM3MzcwODI2NTY5NzM5MA.G52OYp.typWUfgUKAeqLrS0RfMss2_XGTFpwJwMtRJQdw"
-
+import os
+from dotenv import load_dotenv
 
 joke = pyjokes.get_jokes(language='es', category='all')
 intents = discord.Intents.all()
 bot = EasyBot(intents = intents)
 
-
 bot.setCommand('!youtube', 'www.youtube.com')
 bot.setCommand('!instagram','syscursos')
 bot.setCommand('!chiste', joke)
 
+load_dotenv()
+
+token = os.getenv("TOKEN")
 bot.run(token)
